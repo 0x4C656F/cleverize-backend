5961418b2b17de73daa0f70249626dd19d3c1ddc
Lavryniuk Lev <leoyt109@gmail.com>
2023-10-18 12:48:01 +0300

    Stable auth version using express-jwt and jwks-rsa

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthorizationGuard } from './authorization/authorization.guard';
+import { expressjwt, Request as JWTRequest } from 'express-jwt';
 
 @Controller()
 export class AppController {
@@ -8,5 +10,12 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @UseGuards(AuthorizationGuard)
+  @Get('/protected')
+  getPrivate(@Req() request: JWTRequest): string {
+    const auth = request.auth;
+    console.log(auth.sub);
+    return JSON.stringify({ data: 'you have reached unreachable' });
   }
 }
