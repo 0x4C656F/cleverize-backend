b8f567dc0fdc69f647bc51fb1d69f8e76a9e2be5
Lavryniuk Lev <leoyt109@gmail.com>
2023-10-27 17:44:33 +0300

    Added some blank endpoints to users-roadmaps controller

--- src/modules/user-roadmaps/user-roadmaps.controller.ts
+++ src/modules/user-roadmaps/user-roadmaps.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, NotFoundException, Param, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, NotFoundException, Param, Post } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { ApiTags } from "@nestjs/swagger";
 import { Model } from "mongoose";
@@ -28,5 +28,19 @@ export class UserRoadmapsController {
 		if (!roadmap) throw new NotFoundException();
 
 		return roadmap;
+	}
+
+	@Get("users/:userId/roadmaps")
+	public async getAllUserRoadmap(@Param() parameters: OperateUserRoadmapByIdDto) {
+		//this code has to return all user`s roadmaps
+	}
+
+	@Delete("users/:userId/roadmaps/:roadmapId")
+	public async deleteUserRoadmapById(@Param() parameters: OperateUserRoadmapByIdDto) {
+		//this code has to delete user`s roadmap
+		//Actually im not sure whether :userId is needed. The @AuthGuard returns an object with current user data, there is user`s id(user_id).
+		//Since roadmap can be deleted only by it`s owner-
+		//In order to lift the weight from client and move it on backend, i`d suggest we use id provided by authGuard.
+		//It`s negotiatable.
 	}
 }
