6290c10b7e3cb81fd9b0c21cc6e946e1c78a7e8d
async-devil <async.devil@gmail.com>
2023-11-01 15:38:48 +0100

    feat: implement user roadmap CRUD

--- src/modules/user-roadmaps/dtos/create-user-roadmap.dto.ts
+++ src/modules/user-roadmaps/dtos/create-user-roadmap.dto.ts
@@ -1,1 +1,12 @@
-export class CreateUserRoadmapDto {}
+import { ApiProperty, OmitType, PickType } from "@nestjs/swagger";
+import { IsMongoId } from "class-validator";
+
+export class CreateUserRoadmapDto {
+	@IsMongoId()
+	@ApiProperty({ example: "507f191e810c19729de860ea" })
+	public ownerId: string;
+}
+
+export class CreateUserRoadmapParametersDto extends PickType(CreateUserRoadmapDto, ["ownerId"]) {}
+
+export class CreateUserRoadmapBodyDto extends OmitType(CreateUserRoadmapDto, ["ownerId"]) {}
--- src/modules/user-roadmaps/user-roadmaps.schema.ts
+++ src/modules/user-roadmaps/user-roadmaps.schema.ts
@@ -1,10 +1,14 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-import { Document } from "mongoose";
+import { Document, Types } from "mongoose";
+
+import { User } from "../user/entity/user.schema";
 
 @Schema()
 export class UserRoadmap {
-	@Prop({ required: true, unique: true })
-	id: string;
+	public _id: Types.ObjectId;
+
+	@Prop({ required: true, type: Types.ObjectId, ref: User.name })
+	public owner_id: User;
 }
 
 export type UserRoadmapDocument = UserRoadmap & Document;
