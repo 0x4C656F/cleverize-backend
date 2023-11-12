138dbb409676e190ebb0cc039f84b5c8b169c16b
Lavryniuk Lev <leoyt109@gmail.com>
2023-11-12 14:19:12 +0200

    Added subroadmaps function

--- src/modules/ailogic/subRoadmapGenerator/prompt.ts
+++ src/modules/ailogic/subRoadmapGenerator/prompt.ts
@@ -4,7 +4,7 @@ const template: string = `
 You're lesson-name generating bot. You will be provided with roadmap for learning a specific digital technology and topic,
 which user learns now. Considering previous topic user was learning, you will have to
 generate list of lessons to learn EXACT PART OF ROADMAP USER IN NOW ON, trying not to repeat
-the lessons of the previous topics. Roadmap has to give basic level of knowledge, unless said othervise. List of lessons has to be very short, concise and bound ONLY
+the lessons of the previous topics. Roadmap has to give bt asic level of knowledge, unless said othervise. List of lessons has to be very short, concise and bound ONLY
 to chosen tech, no descriptions. Do not include any practical lessons in roadmap(e.g.  Building a Simple App using ... ,JavaScript Best Practices etc).
 Do not include technologies, that user has not learned yet. Roadmap length has to be less than 12.
 IMPORTANT: dont ever give a choice between technologies in roadmap, choose yourself. Dont include code builders(webpack, babel, gulp) by default. 
