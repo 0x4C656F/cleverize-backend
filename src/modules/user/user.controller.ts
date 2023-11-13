e03d31cc676f72c9a0999b02920b4b2e82c866c0
Lavryniukk <leoyt109@gmail.com>
2023-11-13 10:20:57 +0200

    Save commit(achtung, bugs ahead)

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,21 +1,24 @@
-import { Controller, Post, Body, Get, Param, UseGuards } from "@nestjs/common";
+import {
+	Controller,
+	Post,
+	Body,
+	Get,
+	Param,
+	UseGuards,
+	HttpException,
+	HttpStatus,
+} from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { AuthGuard } from "@nestjs/passport";
 // eslint-disable-next-line import/no-extraneous-dependencies
 import axios from "axios";
 import { Model } from "mongoose";
 
+import { JWTPayload, UserPayload } from "src/common/user-payload.decorator";
 import getConfig from "src/config/config";
 
 import { User, UserDocument } from "./entity/user.schema";
 import { UserService } from "./user.service";
-
-// Define the type outside of the class to avoid ESLint errors
-type Auth0Token = {
-	access_token: string;
-	token_type: string;
-	expires_in: number;
-};
 
 @Controller("/users")
 export class UserController {
@@ -44,21 +47,28 @@ export class UserController {
 	}
 	@UseGuards(AuthGuard("jwt"))
 	@Get("/:userId")
+	async verifyAge(@Param('userId') userId: string): Promise<string> {
+
+		
+			const token: string = await this.userService.getManagmentApiToken();
+			
+			const response = await axios({
+				url: `${this.auth0Config.domain}api/v2/users/${userId}`,
+				method: "PATCH",
+				data: { blocked: true },
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+			});
+			return await response.data
+		}
+	}
+
+	@UseGuards(AuthGuard("jwt"))
+	@Get("/:userId")
 	async getUserById(@Param("userId") userId: string): Promise<any> {
-		const options = {
-			method: "POST",
-			url: `${this.auth0Config.domain}oauth/token`,
-			headers: { "content-type": "application/x-www-form-urlencoded" },
-			data: new URLSearchParams({
-				grant_type: "client_credentials",
-				client_id: this.auth0Config.clientId,
-				client_secret: this.auth0Config.clientSecret,
-				audience: this.auth0Config.audience,
-			}),
-		};
-		const axiosResponse = await axios(options);
-		const data: Auth0Token = axiosResponse.data as Auth0Token;
-		const token: string = data.access_token;
+		// eslint-disable-next-line @typescript-eslint/unbound-method
+		const token = await this.userService.getManagmentApiToken();
 		const response = await axios({
 			method: "GET",
 			url: `${this.auth0Config.domain}api/v2/users/${userId}`,
