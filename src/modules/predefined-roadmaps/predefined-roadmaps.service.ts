48dd5788f0067d056288c7f202e9a5b38fef2095
async-devil <async.devil@gmail.com>
2023-11-16 13:24:21 +0100

    feat: update predefined-roadmaps module

--- src/modules/predefined-roadmaps/predefined-roadmaps.service.ts
+++ src/modules/predefined-roadmaps/predefined-roadmaps.service.ts
@@ -3,6 +3,7 @@ import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 
 import { User, UserDocument } from "src/modules/user/entity/user.schema";
+import { UserRoadmap, UserRoadmapDocument } from "src/modules/user-roadmaps/user-roadmaps.schema";
 
 import { PredefinedRoadmap, PredefinedRoadmapDocument } from "./schemas/predefined-roadmap.schema";
 
@@ -10,6 +11,7 @@ import { PredefinedRoadmap, PredefinedRoadmapDocument } from "./schemas/predefin
 export class PredefinedRoadmapsService {
 	constructor(
 		@InjectModel(PredefinedRoadmap.name) private readonly model: Model<PredefinedRoadmapDocument>,
+		@InjectModel(UserRoadmap.name) private readonly userRoadmapModel: Model<UserRoadmapDocument>,
 		@InjectModel(User.name) private readonly userModel: Model<UserDocument>
 	) {}
 
@@ -18,9 +20,21 @@ export class PredefinedRoadmapsService {
 
 		if (!roadmap) throw new NotFoundException("Predefined roadmap not found");
 
+		const roadmapObject = roadmap.toObject();
+
+		const userRoadmapDto = Object.assign(roadmapObject, {
+			isCompleted: false,
+			node_list: roadmapObject.sub_roadmaps_list,
+			_id: undefined,
+		});
+
+		const userRoadmap = new this.userRoadmapModel(userRoadmapDto);
+
+		await userRoadmap.save();
+
 		const updateResult = await this.userModel.findOneAndUpdate(
 			{ user_id: dto.userId },
-			{ $push: { roadmaps: roadmap } },
+			{ $push: { roadmaps: userRoadmap } },
 			{ new: true }
 		);
 
