ede277f2625d552772493612f1b0c904bcffab17
Lavryniukk <leoyt109@gmail.com>
2023-11-24 22:42:55 +0200

    Prompt improvment

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,8 +3,7 @@ import { AuthGuard } from "@nestjs/passport";
 
 import { AppService } from "./app.service";
 import getConfig, { Config } from "./config/config";
-import { startConversation } from "./modules/ailogic/coversations/conversation";
-import generateSubRoadmap from "./modules/ailogic/roadmaps/subRoadmapGenerator/generate-subroadmap";
+
 @Controller()
 export class AppController {
 	private readonly envvars: Config;
--- src/modules/ailogic/roadmaps/subRoadmapGenerator/prompt.ts
+++ src/modules/ailogic/roadmaps/subRoadmapGenerator/prompt.ts
@@ -5,7 +5,8 @@ generate list of lessons to learn EXACT PART OF ROADMAP USER IN NOW ON, trying n
 the lessons of the previous and future topics. Roadmap has to give basic level of knowledge, unless said othervise. List of lessons has to be very short, concise and bound ONLY
 to chosen tech, no descriptions. Do not include any  practical lessons in roadmap(e.g.  Building a Simple App using ... ,JavaScript Best Practices etc).
 Do not include technologies, that user has not learned yet. Lessons list array length has to be less from 8 to 12.
-Don't include explanations such as: JavaScript Control Structures (Conditionals and Loops), instead, write : Control Structures
+Don't include explanations such as: JavaScript Control Structures (Conditionals and Loops), instead, write : Control Structures.
+You must not put slashes('/') in your response.
 IMPORTANT: dont ever give a choice between technologies in roadmap, choose yourself. Dont include code builders(webpack, babel, gulp) by default. 
 
 This is what's your answer should look like:
