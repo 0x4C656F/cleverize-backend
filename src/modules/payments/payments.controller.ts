435d514cc2a262f8cc587fd426e6efeeb876b051
Lavryniukk <leoyt109@gmail.com>
2023-11-26 23:04:56 +0200

    No code

--- src/modules/payments/payments.controller.ts
+++ src/modules/payments/payments.controller.ts
@@ -3,15 +3,15 @@ import Stripe from "stripe";
 
 @Controller("/payments")
 export class PaymentsController {
-	@Get("/pay")
-	async pay() {
-		const stripe = new Stripe(
-			"sk_test_51OEFinCCMdYQSDIPUbKpBEyjIDRxKGh60ydtfsh5QD0g6XMZ9K1VDUEXhbUbWC9ptLBx7J4MtnBl240xDvR5LbVz00Acgca4Xu"
-		);
-		return await stripe.checkout.sessions.create({
-			success_url: "https://www.cleverize.co/",
-			line_items: [{ price: "price_1OGjr6CCMdYQSDIPdpIm2LSR", quantity: 1 }],
-			mode: "subscription",
-		});
-	}
+	// @Get("/pay")
+	// async pay() {
+	// 	const stripe = new Stripe(
+	// 		"sk_test_51OEFinCCMdYQSDIPUbKpBEyjIDRxKGh60ydtfsh5QD0g6XMZ9K1VDUEXhbUbWC9ptLBx7J4MtnBl240xDvR5LbVz00Acgca4Xu"
+	// 	);
+	// 	return await stripe.checkout.sessions.create({
+	// 		success_url: "https://www.cleverize.co/",
+	// 		line_items: [{ price: "price_1OGjr6CCMdYQSDIPdpIm2LSR", quantity: 1 }],
+	// 		mode: "subscription",
+	// 	});
+	// }
 }
