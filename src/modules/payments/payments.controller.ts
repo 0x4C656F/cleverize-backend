302377f19735e2fb8cf6a9796b9f324ca8facdc8
Lavryniukk <leoyt109@gmail.com>
2023-11-26 21:41:16 +0200

    Working on payments

--- src/modules/payments/payments.controller.ts
+++ src/modules/payments/payments.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
+import { ApiBearerAuth } from "@nestjs/swagger";
+import Stripe from "stripe";
 
-@Controller('payments')
-export class PaymentsController {}
+import { JWTPayload, UserPayload } from "src/common/user-payload.decorator";
+
+@Controller("payments")
+export class PaymentsController {
+	@ApiBearerAuth()
+	@UseGuards(AuthGuard("jwt"))
+	@Get("/pay")
+	async pay(@UserPayload() payload: JWTPayload) {
+		const stripe = new Stripe(
+			"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"
+		);
+		return await stripe.checkout.sessions.create({
+			success_url: "https://www.cleverize.co/",
+			line_items: [{ price: "price_H5ggYwtDq4fbrJ", quantity: 1 }],
+			mode: "subscription",
+		});
+	}
+}
