dc81c86c8664ebce0658ecd80a9335f1d9661e12
Lavryniukk <leoyt109@gmail.com>
2023-11-26 22:27:00 +0200

    tilted

--- src/modules/payments/payments.controller.ts
+++ src/modules/payments/payments.controller.ts
@@ -6,11 +6,13 @@ import Stripe from "stripe";
 import getConfig from "../../config/config";
 @Controller("payments")
 export class PaymentsController {
-	private config = getConfig();
+	// private config = getConfig();
 
 	@Get("/pay")
 	async pay() {
-		const stripe = new Stripe(this.config.stripe);
+		const stripe = new Stripe(
+			"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"
+		);
 		return await stripe.checkout.sessions.create({
 			success_url: "https://www.cleverize.co/",
 			line_items: [{ price: "price_1OGjr6CCMdYQSDIPdpIm2LSR", quantity: 1 }],
