48d6316984eeb2191445f1e8d0e158b1ac6394f2
Lavryniukk <leoyt109@gmail.com>
2023-11-26 22:03:34 +0200

    Bug fix

--- src/modules/payments/payments.controller.ts
+++ src/modules/payments/payments.controller.ts
@@ -1,22 +1,18 @@
-import { Body, Controller, Get, Post, UseGuards } from "@nestjs/common";
-import { AuthGuard } from "@nestjs/passport";
-import { ApiBearerAuth } from "@nestjs/swagger";
+import { Body, Controller, Get } from "@nestjs/common";
 import Stripe from "stripe";
 
 import { JWTPayload, UserPayload } from "src/common/user-payload.decorator";
 
 @Controller("payments")
 export class PaymentsController {
-	@ApiBearerAuth()
-	@UseGuards(AuthGuard("jwt"))
 	@Get("/pay")
-	async pay(@UserPayload() payload: JWTPayload) {
+	async pay() {
 		const stripe = new Stripe(
-			"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"
+			"sk_test_51OEFinCCMdYQSDIPUbKpBEyjIDRxKGh60ydtfsh5QD0g6XMZ9K1VDUEXhbUbWC9ptLBx7J4MtnBl240xDvR5LbVz00Acgca4Xu"
 		);
 		return await stripe.checkout.sessions.create({
 			success_url: "https://www.cleverize.co/",
-			line_items: [{ price: "price_H5ggYwtDq4fbrJ", quantity: 1 }],
+			line_items: [{ price: "price_1OGjr6CCMdYQSDIPdpIm2LSR", quantity: 1 }],
 			mode: "subscription",
 		});
 	}
