dff726258c2ce5bc5d3268342491ea75743bd01b
Lavryniukk <leoyt109@gmail.com>
2023-11-26 23:10:36 +0200

    Moved stripe to app controller, removed payments controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Get, Post, UseGuards } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
+import Stripe from "stripe";
 
 import { AppService } from "./app.service";
 import getConfig, { Config } from "./config/config";
@@ -16,7 +17,17 @@ export class AppController {
 		console.log("touched");
 		return "OK";
 	}
-
+	@Get("/pay")
+	async pay() {
+		const stripe = new Stripe(
+			"sk_test_51OEFinCCMdYQSDIPUbKpBEyjIDRxKGh60ydtfsh5QD0g6XMZ9K1VDUEXhbUbWC9ptLBx7J4MtnBl240xDvR5LbVz00Acgca4Xu"
+		);
+		return await stripe.checkout.sessions.create({
+			success_url: "https://www.cleverize.co/",
+			line_items: [{ price: "price_1OGjr6CCMdYQSDIPdpIm2LSR", quantity: 1 }],
+			mode: "subscription",
+		});
+	}
 	@UseGuards(AuthGuard("jwt"))
 	@Get("/health/protected")
 	protected() {
