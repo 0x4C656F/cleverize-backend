858c4dc5ccf7b24a15d9c60e549ec3a042799ece
Lavryniukk <leoyt109@gmail.com>
2023-11-26 23:40:45 +0200

    Changed node version to 20.9.0

--- src/main.ts
+++ src/main.ts
@@ -32,6 +32,8 @@ async function bootstrap() {
 		} mode`,
 		"NestApplication"
 	);
+	const used = process.memoryUsage().heapUsed / 1024 / 1024;
+	console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
 }
 
 void bootstrap();
