71c91c629a86bd3e760c2d6d5b13036fe68ba88d
Lavryniukk <leoyt109@gmail.com>
2023-12-03 21:01:04 +0200

    pi

--- src/modules/ailogic/roadmaps/subRoadmapGenerator/prompt.ts
+++ src/modules/ailogic/roadmaps/subRoadmapGenerator/prompt.ts
@@ -3,13 +3,13 @@ You're roadmap generating bot. You will be provided with roadmap for learning a 
 which user learns now. Considering previous topics user was learning, you will have to
 generate list of lessons to learn EXACT TOPIC OF ROADMAP USER IN NOW ON(if user's topic is SQL, generate roadmap for SQL. If it says javascript basics, generate roadmap for javascript basics), trying not to repeat
 the lessons of the previous and future topics. Roadmap has to give basic level of knowledge.
-First roadmap node must  be intro and install.
+First node should always be introduction.
 List of lessons has to be very short, concise and bound ONLY
 to chosen tech.Do not provide descriptions, explanations or anything but tech names for roadmap.  Do not include any  practical lessons in roadmap(e.g.  Building a Simple App using ... ,JavaScript Best Practices etc).
 Do not include technologies, that user has not learned yet. Lessons list array length has to be less than 12.
 Don't include explanations such as: JavaScript Control Structures (Conditionals and Loops), instead, just write : Control Structures.
 You must not put any special characters and punctuation marks in your response.
-IMPORTANT: dont ever give a choice between technologies in roadmap, choose yourself. Dont include code builders(webpack, babel, gulp) by default. 
+IMPORTANT: don't ever give a choice between technologies in roadmap, choose yourself. Don't include code builders(webpack, babel, gulp) by default. 
 
 This is what's your answer should look like:
 {
