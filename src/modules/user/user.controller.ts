6a265ea87b1886baddf936386e212908bac8c7e5
Lavryniukk <leoyt109@gmail.com>
2023-12-15 20:43:53 +0200

    debug

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -32,6 +32,7 @@ export class UserController {
 	@UseGuards(AuthGuard("jwt"))
 	async getCreditsByUserId(@UserPayload() payload: JWTPayload): Promise<number | undefined> {
 		const user = await this.userModel.findOne({ user_id: payload.sub });
+		console.log(user);
 		return user.credits;
 	}
 
