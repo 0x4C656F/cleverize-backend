eb027b7caa274f18fb7dd093494b617e3d52ac92
Lavryniukk <leoyt109@gmail.com>
2023-12-15 12:06:56 +0200

    Added the smallest credit support

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,12 +1,21 @@
 import clerkClient from "@clerk/clerk-sdk-node";
-import { Controller, Post, Body, Get, Param, UseGuards } from "@nestjs/common";
+import { Controller, Post, Body, Get, Param, UseGuards, Inject } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
 import { AuthGuard } from "@nestjs/passport";
+import { ApiBearerAuth } from "@nestjs/swagger";
+import { Model } from "mongoose";
 
+import { JWTPayload, UserPayload } from "src/common/user-payload.decorator";
+
+import { User, UserDocument } from "./entity/user.schema";
 import { UserService } from "./user.service";
 
 @Controller("/users")
 export class UserController {
-	constructor(private readonly userService: UserService) {}
+	constructor(
+		private readonly userService: UserService,
+		@InjectModel(User.name) private readonly userModel: Model<UserDocument>
+	) {}
 
 	@Post()
 	async upsertUser(@Body() userData: { data: { user_id: string } }): Promise<any> {
@@ -16,6 +25,14 @@ export class UserController {
 	@Get("/all")
 	async findAll(): Promise<any> {
 		return this.userService.findAll();
+	}
+
+	@Get("/me/credits")
+	@ApiBearerAuth()
+	@UseGuards(AuthGuard("jwt"))
+	async getCreditsByUserId(@UserPayload() payload: JWTPayload): Promise<number | undefined> {
+		const user = await this.userModel.findById(payload.sub);
+		return user.credits;
 	}
 
 	@UseGuards(AuthGuard("jwt"))
