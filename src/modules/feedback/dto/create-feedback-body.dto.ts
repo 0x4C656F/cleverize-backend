c5de902eb39598a9ad7f461da7a46b491c7651d5
Lavryniukk <leoyt109@gmail.com>
2024-02-15 02:52:58 +0200

    Refactor CreateFeedbackBodyDto to include validation

--- src/modules/feedback/dto/create-feedback-body.dto.ts
+++ src/modules/feedback/dto/create-feedback-body.dto.ts
@@ -1,18 +1,20 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, ValidateIf } from "class-validator";
 
 export class CreateFeedbackBodyDto {
-
-
-	@ApiProperty({ example: "507f191e810c19729de860ea" })
+    @ApiProperty({ example: "507f191e810c19729de860ea" })
+    @ValidateIf((o: CreateFeedbackBodyDto) => !o.roadmap_id)
+    @IsNotEmpty()
 	public conversation_id?: string;
 
 	@ApiProperty({ example: "507f191e810c19729de860ea" })
+	@ValidateIf((o: CreateFeedbackBodyDto) => !o.conversation_id)
+	@IsNotEmpty()
 	public roadmap_id?: string;
 
 	@ApiProperty({ example: "I think that" })
 	public feedback: string;
 
-	@ApiProperty({ example: 4, type: Number, minimum: 0, maximum: 5})
+	@ApiProperty({ example: 4, type: Number, minimum: 0, maximum: 5 })
 	public rating: number;
 }
-
