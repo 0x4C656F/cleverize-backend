ea22d4187f818b6850030685ceffeb235344dee9
Lavryniukk <leoyt109@gmail.com>
2024-02-15 03:33:09 +0200

    Remove unused validator constraint

--- src/modules/feedback/dto/create-feedback-body.dto.ts
+++ src/modules/feedback/dto/create-feedback-body.dto.ts
@@ -1,22 +1,4 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { Validate, ValidatorConstraint, ValidatorConstraintInterface } from "class-validator";
-
-@ValidatorConstraint({ async: false })
-export class IsEitherConversationOrRoadmapPresent implements ValidatorConstraintInterface {
-	validate(value: {
-		conversation_id: string | undefined;
-		roadmap_id: string | undefined;
-	}): boolean {
-		return !!(
-			(value.conversation_id && !value.roadmap_id) ||
-			(!value.conversation_id && value.roadmap_id)
-		);
-	}
-
-	defaultMessage() {
-		return "Either conversation_id or roadmap_id must be present, but not both";
-	}
-}
 
 export class CreateFeedbackBodyDto {
 	@ApiProperty({ example: "I think that" })
@@ -31,6 +13,5 @@ export class CreateFeedbackBodyDto {
 	@ApiProperty({ example: "507f191e810c19729de860ea" })
 	public roadmap_id?: string;
 
-	@Validate(IsEitherConversationOrRoadmapPresent)
 	public eitherConversationOrRoadmap: any;
 }
