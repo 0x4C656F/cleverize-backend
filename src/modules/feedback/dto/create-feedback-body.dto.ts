addbe5536b221a29e030159d49800be35cbb27e4
Lavryniukk <leoyt109@gmail.com>
2024-02-15 03:23:22 +0200

    Refactor CreateFeedbackBodyDto to include a custom validator for either conversation_id or roadmap_id

--- src/modules/feedback/dto/create-feedback-body.dto.ts
+++ src/modules/feedback/dto/create-feedback-body.dto.ts
@@ -1,22 +1,34 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsMongoId, IsNotEmpty, ValidateIf } from "class-validator";
+import { Validate, ValidatorConstraint, ValidatorConstraintInterface } from "class-validator";
+
+@ValidatorConstraint({ async: false })
+export class IsEitherConversationOrRoadmapPresent implements ValidatorConstraintInterface {
+	validate(value: any): boolean {
+		const { conversation_id, roadmap_id } = value as {
+			conversation_id?: string;
+			roadmap_id?: string;
+		};
+		return !!((conversation_id && !roadmap_id) || (!conversation_id && roadmap_id));
+	}
+
+	defaultMessage() {
+		return "Either conversation_id or roadmap_id must be present, but not both";
+	}
+}
 
 export class CreateFeedbackBodyDto {
-
-
 	@ApiProperty({ example: "I think that" })
 	public feedback: string;
 
 	@ApiProperty({ example: 4, type: Number, minimum: 0, maximum: 5, required: true })
 	public rating: number;
 
-	@IsMongoId()
-	@ValidateIf((o: CreateFeedbackBodyDto) => !o.roadmap_id)
 	@ApiProperty({ example: "507f191e810c19729de860ea" })
 	public conversation_id?: string;
 
-	@IsMongoId()
-	@ValidateIf((o: CreateFeedbackBodyDto) => !o.conversation_id)
 	@ApiProperty({ example: "507f191e810c19729de860ea" })
 	public roadmap_id?: string;
+
+	@Validate(IsEitherConversationOrRoadmapPresent)
+	public eitherConversationOrRoadmap: any;
 }
