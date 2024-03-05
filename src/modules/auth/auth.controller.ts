111918ea7bddfebdfeeef505d821ee734adb10cf
Lavryniukk <leoyt109@gmail.com>
2024-03-05 18:33:31 +0200

    Refactor auth.controller.ts to use @Res({ passthrough: true }) decorator

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -12,23 +12,25 @@ export class AuthController {
 	constructor(private readonly authService: AuthService) {}
 
 	@Post("sign-up")
-	async signUp(@Res() response: Response, @Body() dto: SignUpDto) {
+	async signUp(@Res({ passthrough: true }) response: Response, @Body() dto: SignUpDto) {
 		return this.authService.registerUser(response, dto);
 	}
 
 	@Post("sign-in")
-	async signIn(@Res() response: Response, @Body() dto: SignInDto) {
+	async signIn(@Res({ passthrough: true }) response: Response, @Body() dto: SignInDto) {
 		return this.authService.loginUser(response, dto);
 	}
 
 	@Post("logout")
-	logout(@Res() response: Response) {
+	logout(@Res({ passthrough: true }) response: Response) {
 		return this.authService.logout(response);
 	}
 
 	@Post("refresh-token")
-	async refresh(@Res() response: Response, @Cookies("refresh_token") token: string) {
+	async refresh(
+		@Res({ passthrough: true }) response: Response,
+		@Cookies("refresh_token") token: string
+	) {
 		return this.authService.refreshTokens(response, token);
 	}
-	
 }
