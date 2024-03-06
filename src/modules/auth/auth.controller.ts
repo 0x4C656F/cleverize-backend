203b14b330d583bd6b68fc874fd95d35e982ade4
Lavryniukk <leoyt109@gmail.com>
2024-03-06 22:48:22 +0200

    Add Access-Control-Allow-Origin header and update cookie settings in signUp method

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Res } from "@nestjs/common";
+import { Body, Controller, Header, Logger, Post, Res } from "@nestjs/common";
 import { Response } from "express";
 
 import { Cookies } from "src/common/cookies.decorator";
@@ -11,43 +11,23 @@ import { SignUpDto } from "./dto/sign-up.dto";
 export class AuthController {
 	constructor(private readonly authService: AuthService) {}
 
+	@Header("Access-Control-Allow-Origin", "*")
 	@Post("sign-up")
-	async signUp(@Res() response: Response, @Body() dto: SignUpDto, @Cookies() token: string) {
+	async signUp(@Res({passthrough: true}) response: Response, @Body() dto: SignUpDto, @Cookies() token: string) {
 		console.log("Received cookies", token);
 		const { access_token, refresh_token } = await this.authService.registerUser(response, dto);
-		response.setHeader("Access-Control-Allow-Credentials", "true");
-		response.setHeader(
-			"Access-Control-Allow-Origin",
-			"https://vercel.live/link/cleverize-git-auth-rework-lavryniukk.vercel.app?via=deployment-domains-list-branch"
-		);
-		response.setHeader("Access-Control-Expose-Headers", "Set-Cookie");
-		// response.cookie("access_token", access_token, {
-		// 	maxAge: 1000 * 60 * 60,
-		// 	sameSite: "none",
-		// 	secure: true,
-		// });
-		const maxAge = 3600; // Max-Age in seconds
-		const expires = new Date(Date.now() + maxAge * 1000).toUTCString(); // Convert Max-Age to Expires date
 
-		response.setHeader(
-			"Set-Cookie",
-			`access_token=${access_token}; Max-Age=${maxAge}; Path=/; Expires=${expires}; Secure; SameSite=None`
-		);
-		const maxAge1 = 3600; // Max-Age in seconds
-		const expires1 = new Date(Date.now() + maxAge * 1000).toUTCString(); // Convert Max-Age to Expires date
+		response.cookie("access_token", access_token, {
+			maxAge: 1000 * 60 * 60,
+			sameSite: "strict",
+		});
+		response.cookie("refresh_token", refresh_token, {
+			httpOnly: true,
+			maxAge: 1000 * 60 * 60 * 24 * 7,
+			sameSite: "strict",
+		});
 
-		response.setHeader(
-			"Set-Cookie",
-			`refresh_token=${refresh_token}; Max-Age=${maxAge}; Path=/; Expires=${expires}; Secure; SameSite=None`
-		);
-		// response.cookie("refresh_token", refresh_token, {
-		// 	httpOnly: true,
-		// 	maxAge: 1000 * 60 * 60 * 24 * 7,
-		// 	sameSite: "none",
-		// 	secure: true,
-		// });
-
-		response.status(200).send("OK");
+		return 'User created';
 	}
 
 	@Post("sign-in")
